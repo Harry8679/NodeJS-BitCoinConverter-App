9980fe5885f3bed1978f2929206a008a7cf681f1
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-17 00:46:47 +0100

    Management of errors

--- btc.js
+++ btc.js
@@ -1,13 +1,25 @@
 const axios = require('axios');
 
 const main = async () => {
-    const currency = process.argv[2];
-    // console.log(process.argv);
-    const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+    try {
+        let currency = 'EUR';
 
-    const updatedAt = response.data.time.updated;
-    const rate = response.data.bpi[currency].rate;
-    console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
+        if (process.argv[2]) {
+            currency = process.argv[2].toUpperCase();
+        }
+        // console.log(process.argv);
+        const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+        if (!response.data.bpi[currency]) {
+            throw new Error('Devise inconnue');
+        }
+        
+        const updatedAt = response.data.time.updated;
+        const rate = response.data.bpi[currency].rate;
+        console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
+    } catch (err) {
+        console.log(err.toString());
+    }
 }
 
 main();
