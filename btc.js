5eaa8a6c7ca08f686b6444636def26c9e7ee3e58
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-16 23:35:41 +0100

    Get the currency by the argv

--- btc.js
+++ btc.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 
 const main = async () => {
+    const currency = process.argv[2];
+    // console.log(process.argv);
     const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
 
     const updatedAt = response.data.time.updated;
-    const rate = response.data.bpi['EUR'].rate;
-    console.log(`> 1 BTC = ${rate} EUR (${updatedAt})`);
+    const rate = response.data.bpi[currency].rate;
+    console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
 }
 
 main();
