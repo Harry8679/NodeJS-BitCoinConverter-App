9b4959fe69879980ae36c988800835ee30edf391
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-17 02:01:34 +0100

    Code Rfactoring

--- btc.js
+++ btc.js
@@ -1,21 +1,24 @@
 const axios = require('axios');
 
 const main = async () => {
+    const currency = process.argv[2] ? process.argv[2] : 'EUR';
+
     try {
-        let currency = 'EUR';
+        const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+        // let currency = 'EUR';
 
-        if (process.argv[2]) {
-            currency = process.argv[2].toUpperCase();
-        }
+        // if (process.argv[2]) {
+        //     currency = process.argv[2].toUpperCase();
+        // }
         // console.log(process.argv);
-        const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+        const { data } = await axios.get(url);
 
-        if (!response.data.bpi[currency]) {
-            throw new Error('Devise inconnue');
+        if (!data.bpi[currency]) {
+            throw new Error('Devise inconnue : USD, EUR or GBP');
         }
         
-        const updatedAt = response.data.time.updated;
-        const rate = response.data.bpi[currency].rate;
+        const updatedAt = data.time.updated;
+        const rate = data.bpi[currency].rate;
         console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
     } catch (err) {
         console.log(err.toString());
